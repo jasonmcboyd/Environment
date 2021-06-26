b2d3fcebe1efa73893ca6116c832b028fb111bc3
Jason Boyd <jason.boyd.ce@gmail.com>
2021-06-26 16:00:32 -0700

    Added based 64 functions to PowerShell profile

---
+++ PowerShell/Profiles/CurrentUserAllHosts/Imports/base64.ps1
@@ -0,0 +1,27 @@
+function ConvertTo-Base64Encoding {
+	param (
+		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[string[]]
+		$Value
+	)
+
+	Process {
+		foreach ($val in $Value) {
+			[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($val))
+		}
+	}
+}
+
+function ConvertFrom-Base64Encoding {
+	param (
+		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[string[]]
+		$Value
+	)
+
+	Process {
+		foreach ($val in $Value) {
+			[System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($val))
+		}
+	}
+}
