de3ce0fe36a61aaa8c0a5d04454e934a79b534bf
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-01 22:03:24 -0700

    Removed debugging code I accidently commited

--- PowerShellProfiles/CurrentUserAllHosts/Imports/watch.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/watch.ps1
@@ -13,11 +13,7 @@ function watch {
         $($ScriptBlock.ToString())
 "@
 
-    Write-Host $command
-
     $encoded = ConvertTo-Base64Encoding $command
-
-    Write-Host "encoded: $encoded"
 
     wsl watch --color --interval $Interval "'/mnt/c/Program Files/PowerShell/7/pwsh.exe' -e '$encoded' -o Text"
 }
