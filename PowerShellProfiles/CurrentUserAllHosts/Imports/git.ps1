0add71c7a605f9a5d4d26ac58e12784a462f071f
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-02 09:44:51 -0700

    Fixed Git-Graph function, it had been a complete mess

--- PowerShellProfiles/CurrentUserAllHosts/Imports/git.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/git.ps1
@@ -1,23 +1,23 @@
 function Git-Graph {
     [CmdletBinding()]
     param (
-        [Parameter(Position = 0, ParameterSetName = 'TruncatedBranch')]
-        [Parameter(Position = 0, ParameterSetName = 'FullBranch')]
+        [Parameter(Position = 0, ParameterSetName = 'SpecificBranch, TruncatedLogs')]
+        [Parameter(Position = 0, ParameterSetName = 'SpecificBranch, FullLogs')]
         [string[]]
         $Branch = '',
 
-        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'TruncatedAllBranches')]
-        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'FullAllBranches')]
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'AllBranches, TruncatedLogs')]
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'AllBranches, FullLogs')]
         [switch]
         $All,
 
-        [Parameter(Position = 1, ParameterSetName = 'TruncatedBranch')]
-        [Parameter(Position = 1, ParameterSetName = 'TruncatedAllBranches')]
+        [Parameter(Position = 1, ParameterSetName = 'SpecificBranch, TruncatedLogs')]
+        [Parameter(Position = 1, ParameterSetName = 'AllBranches, TruncatedLogs')]
         [int]
         $Number = 10,
 
-        [Parameter(Position = 1, Mandatory = $true, ParameterSetName = 'FullBranch')]
-        [Parameter(Position = 1, Mandatory = $true, ParameterSetName = 'FullAllBranches')]
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName = 'SpecificBranch, FullLogs')]
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName = 'AllBranches, FullLogs')]
         [switch]
         $Full,
 
@@ -26,26 +26,30 @@ function Git-Graph {
         $AdditionalGitParameters
     )
 
+    Set-StrictMode -Version 'latest'
+    $ErrorActionPreference = 'Stop'
+
     Write-Debug "ParamterSetName: $($PSCmdlet.ParameterSetName)"
 
-    if ($PSCmdlet.ParameterSetName -eq 'AllBranches') {
+    if ($PSCmdlet.ParameterSetName -like '*AllBranches*') {
         $branches = '--all' 
     }
     else {
-        if (![string]::IsNullOrWhiteSpace('Branch')) {
-            $branches = $Branch
-        }
-        else {
-            $branches = & git branch --show-current
-        }
+        $branches = $Branch
     }
 
     Write-Debug "branches: $branches"
 
-    $command = 'git log --graph --oneline'
-    $command += " $AdditionalGitParameters"
-    if ($PSBoundParameters.ContainsKey('Number')) { $command += " -n $Number" }
-    $command += $brances
+    $tokens = @(
+        'git log --graph --oneline'
+    )
+    if (![string]::IsNullOrEmpty($AdditionalGitParameters)) { $tokens += $AdditionalGitParameters }
+    if ($PSCmdlet.ParameterSetName -like '*TruncatedLogs*') { $tokens += "-n $Number" }
+    if (![string]::IsNullOrEmpty($branches)) { $tokens += $branches }
+
+    $command = [string]::Join(' ', $tokens)
+
+    Write-Debug "AdditionalGitParameters: $AdditionalGitParameters"
 
     Write-Debug "command: $command"
 
@@ -58,3 +62,4 @@ function gb { & git branch }
 function wgg { watch { Git-Graph -Full -AdditionalGitParameters '--color --decorate --all' } }
 
 Set-Alias -Name gg -Value Git-Graph
+
