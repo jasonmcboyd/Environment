7d693ab36a5733f6c155d3532e24e424d4642a7e
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-01 22:01:21 -0700

    Improvements to watch function

--- PowerShellProfiles/CurrentUserAllHosts/Imports/watch.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/watch.ps1
@@ -7,5 +7,17 @@ function watch {
         $Interval = 2
     )
 
-    wsl watch --color --interval $Interval "'/mnt/c/Program Files/PowerShell/7/pwsh.exe' -C '$ScriptBlock'"
+    $command = @"
+        Write-Host '$($ScriptBlock.ToString())'
+        Write-Host ''
+        $($ScriptBlock.ToString())
+"@
+
+    Write-Host $command
+
+    $encoded = ConvertTo-Base64Encoding $command
+
+    Write-Host "encoded: $encoded"
+
+    wsl watch --color --interval $Interval "'/mnt/c/Program Files/PowerShell/7/pwsh.exe' -e '$encoded' -o Text"
 }
