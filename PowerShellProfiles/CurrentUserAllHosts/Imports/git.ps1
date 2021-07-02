eac5d2d03b850ca546e6dd4741000670fdb73d01
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-01 22:02:45 -0700

    Improved wgg function

--- PowerShellProfiles/CurrentUserAllHosts/Imports/git.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/git.ps1
@@ -55,6 +55,6 @@ function Git-Graph {
 function gs { & git status }
 function gb { & git branch }
 
-function wgg { watch { Git-Graph -Full -AdditionalGitParameters '--color --decorate' } }
+function wgg { watch { Git-Graph -Full -AdditionalGitParameters '--color --decorate --all' } }
 
 Set-Alias -Name gg -Value Git-Graph
