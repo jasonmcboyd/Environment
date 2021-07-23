b2eebf286d870c90d25d3a4c96da99fc79decb47
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-23 14:05:33 -0700

    Updated Import-VimrcFromGitHub command to check for multiple different home directories

--- PowerShellProfiles/CurrentUserAllHosts/Imports/Import-VimrcFromGitHub.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/Import-VimrcFromGitHub.ps1
@@ -5,6 +5,19 @@ function Import-VimrcFromGitHub {
         $Branch = 'master'
     )
 
-    Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.vimrc" -OutFile ~/.vimrc
-    Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.viemurc" -OutFile ~/.viemurc
+    $userHome = $HOME
+
+    Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.vimrc" -OutFile "$userHome/.vimrc"
+    Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.viemurc" -OutFile "$userHome/.viemurc"
+
+    if (Test-Path Env:HOME) {
+        $path = $env:HOME
+        Copy-Item "$userHome/.vimrc" "$path/.vimrc"
+        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+    }
+    elseif ((Test-Path Env:\HOMEDRIVE) -and (Test-Path Env:\HOMEPATH)) {
+        $path = Join-Path $env:HOMEDRIVE $env:HOMEPATH
+        Copy-Item "$userHome/.vimrc" "$path/.vimrc"
+        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+    }
 }
