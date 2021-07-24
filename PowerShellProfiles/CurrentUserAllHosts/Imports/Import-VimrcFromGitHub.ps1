3cc530baf4212ce59eb5dba55e5c14ed42a068dd
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-24 12:13:23 -0700

    Fixed bug in vim import script

--- PowerShellProfiles/CurrentUserAllHosts/Imports/Import-VimrcFromGitHub.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/Imports/Import-VimrcFromGitHub.ps1
@@ -7,17 +7,31 @@ function Import-VimrcFromGitHub {
 
     $userHome = $HOME
 
+    Write-Debug "userHome: $userHome"
+
     Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.vimrc" -OutFile "$userHome/.vimrc"
     Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.viemurc" -OutFile "$userHome/.viemurc"
 
-    if (Test-Path Env:HOME) {
+    if (Test-Path Env:\HOME) {
+
         $path = $env:HOME
-        Copy-Item "$userHome/.vimrc" "$path/.vimrc"
-        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+
+        Write-Debug "Env:\HOME exists, path is $path."
+
+        if ($path -ne $userHome) {
+            Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+            Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+        }
     }
     elseif ((Test-Path Env:\HOMEDRIVE) -and (Test-Path Env:\HOMEPATH)) {
+
         $path = Join-Path $env:HOMEDRIVE $env:HOMEPATH
-        Copy-Item "$userHome/.vimrc" "$path/.vimrc"
-        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+
+        Write-Debug "Env:\HOMEDRIVE and Env:\HOMEPATH exists, path is $path."
+
+        if ($path -ne $userHome) {
+            Copy-Item "$userHome/.vimrc" "$path/.vimrc"
+            Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+        }
     }
 }
