e11b714965a4c2395b9a78a2db6bed2f87563bbe
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-25 14:37:32 -0700

    Updated vim import script to support file hash comparisons

--- PowerShellProfiles/CurrentUserAllHosts/NoImports/Compare-FileHash.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/NoImports/Compare-FileHash.ps1
@@ -13,7 +13,6 @@ param (
 )
 
 $localFileHash = Get-FileHash -Path $FilePath | Select-Object -ExpandProperty Hash
-Write-Debug "localFileHash: $localFileHash"
 
 $rootUrl = & "$PSScriptRoot/Get-GitHubRootUrl.ps1" -Branch $Branch
 
@@ -24,13 +23,12 @@ $remoteFileHash =
   $RemoteFileHashes `
   | Where-Object { $_.RelativePath -eq $fileHashLookupKey } `
   | Select-Object -ExpandProperty FileHash
-Write-Debug "remoteFileHash: $remoteFileHash"
 
 $hashesMatch = $localFileHash -eq $remoteFileHash
-Write-Verbose @"
+Write-Debug @"
 Hash Comparison:
-  $FilePath : $localFileHash
-  $GitHuburl : $remoteFileHash
-  Match : $hashesMatch
+    $FilePath : $localFileHash
+    $GitHuburl : $remoteFileHash
+    Match : $hashesMatch
 "@
 $hashesMatch
--- PowerShellProfiles/CurrentUserAllHosts/NoImports/Import-VimrcFromGitHub.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/NoImports/Import-VimrcFromGitHub.ps1
@@ -6,32 +6,54 @@ param (
     $RemoteFileHashes
 )
 
-$userHome = $HOME
-
-Write-Debug "userHome: $userHome"
-
-Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.vimrc" -OutFile "$userHome/.vimrc"
-Invoke-WebRequest "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/Vim/.viemurc" -OutFile "$userHome/.viemurc"
+$targetDirectories = [System.Collections.Generic.HashSet[string]]::new()
+$targetDirectories.Add($HOME)
 
 if (Test-Path Env:\HOME) {
-
     $path = $env:HOME
 
     Write-Debug "Env:\HOME exists, path is $path."
 
-    if ($path -ne $userHome) {
-        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
-        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
-    }
+    $targetDirectories.Add($path)
 }
 elseif ((Test-Path Env:\HOMEDRIVE) -and (Test-Path Env:\HOMEPATH)) {
-
     $path = Join-Path $env:HOMEDRIVE $env:HOMEPATH
 
     Write-Debug "Env:\HOMEDRIVE and Env:\HOMEPATH exists, path is $path."
 
-    if ($path -ne $userHome) {
-        Copy-Item "$userHome/.vimrc" "$path/.vimrc"
-        Copy-Item "$userHome/.viemurc" "$path/.viemurc"
+    $targetDirectories.Add($path)
+}
+
+Write-Debug "targetDirectories:`r`n$targetDirectories"
+Write-Debug $targetDirectories.Length
+
+if ($targetDirectories.Length -eq 0) {
+    throw "No target directories defined"
+}
+
+$rootUrl = & "$PSScriptRoot/Get-GitHubRootUrl.ps1" -Branch $Branch
+$files = @('.vimrc', '.viemurc')
+
+foreach ($file in $files) {
+    $url = "$rootUrl/Vim/$file"
+    Write-Debug "url: $url"
+
+    $destination = "$($targetDirectories[0])/$file"
+    Write-Debug "destination: $destination"
+
+    $hashesMatch =
+        & "$PSScriptRoot/Compare-FileHash.ps1" `
+        -Branch $Branch `
+        -FilePath $destination `
+        -GitHubUrl $url `
+        -RemoteFileHashes $RemoteFileHashes
+
+    if (!$hashesMatch) {
+        Write-Verbose "Downloading $url"
+        Invoke-WebRequest $url -OutFile "$destination"
+
+        foreach ($targetDirectory in $targetDirectories | Select-Object -Skip 1) {
+            Copy-Item $destination "$targetDirectory/$file"
+        }
     }
 }
