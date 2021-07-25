c8d12b9f6a8b7e55149d55bf8090014dd551108e
Jason Boyd <jason.boyd.ce@gmail.com>
2021-07-25 10:21:25 -0700

    Forgot to download the NoImports folder

--- PowerShellProfiles/CurrentUserAllHosts/NoImports/Import-ProfileFromGitHub.ps1
+++ PowerShellProfiles/CurrentUserAllHosts/NoImports/Import-ProfileFromGitHub.ps1
@@ -9,14 +9,22 @@ function Import-ProfileFromGitHub {
     Write-Debug "url: $url"
 
     $remoteImportsDirectory = 'PowerShellProfiles/CurrentUserAllHosts/Imports'
+    $remoteNoImportsDirectory = 'PowerShellProfiles/CurrentUserAllHosts/NoImports'
     $localProfileDirectory = Split-Path $profile.CurrentUserAllHosts
     $localImportsDirectory = Join-Path $localProfileDirectory 'Imports/Personal'
+    $localNoImportsDirectory = Join-Path $localProfileDirectory 'NoImports'
 
     Write-Debug "localImportsDirectory: $localImportsDirectory"
+    Write-Debug "localNoImportsDirectory: $localNoImportsDirectory"
 
     if (!(Test-Path $localImportsDirectory)) {
-        Write-Verbose "Creatimg local imports directory..."
+        Write-Verbose "Creatimg local Imports directory..."
         mkdir $localImportsDirectory
+    }
+
+    if (!(Test-Path $localNoImportsDirectory)) {
+        Write-Verbose "Creatimg local NoImports directory..."
+        mkdir $localNoImportsDirectory
     }
 
     $directoryStructure = `
@@ -33,7 +41,20 @@ function Import-ProfileFromGitHub {
         $destination = Join-Path $localImportsDirectory $filename
         Write-Debug "destination: $destination"
 
-        $url = "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/PowerShellProfiles/CurrentUserAllHosts/Imports/$filename"
+        $url = "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/$remoteImportsDirectory/$filename"
+        Write-Debug "url: $url"
+
+        Invoke-WebRequest -Uri $url -OutFile $destination
+    }
+
+    $filesToImport = $directoryStructure.tree.path | Where-Object { $_ -like "$remoteNoImportsDirectory/*.ps1" }
+
+    foreach ($fileToImport in $filesToImport) {
+        $filename = Split-Path -Path $fileToImport -Leaf
+        $destination = Join-Path $localNoImportsDirectory $filename
+        Write-Debug "destination: $destination"
+
+        $url = "https://raw.githubusercontent.com/jasonmcboyd/Environment/$Branch/$remoteNoImportsDirectory/$filename"
         Write-Debug "url: $url"
 
         Invoke-WebRequest -Uri $url -OutFile $destination
